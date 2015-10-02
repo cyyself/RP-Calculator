230e55fdce0f584c0794d1dd85aad411301e7b4b
Yangyu Chen <cyy@YangyudeMacBook-Pro.local>
2015-10-02 22:07:56 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+long rp(string a){
+	long c;
+	for (int i = 0; i <a.length(); ++i)
+	{
+		c+=a[i];
+		//cout << a[i] << " Code=" << int(a[i]) << endl;
+	}
+	c = abs(c);
+	return c % 100;
+}
+
+int main(){
+	string name;
+	cout << "请输入姓名：";
+	cin >> name;
+	int rpv = 100 - rp(name);
+	cout <<name << "的人品值为:" << rpv  << endl;
+	return 0;
+}
+
